94b167296b6d020536d64121b318ddf9a638ad2f
unknown <sendtomadnan@gmail.com>
2025-12-11 18:46:32 +0500

    fix side bar

--- src/layouts/styles.ts
+++ src/layouts/styles.ts
@@ -14,20 +14,23 @@ export const LayoutWrapper = styled.div`
 export const ContentArea = styled.div<ContentAreaProps>`
   margin-top: 35px;
   margin-left: 0;
-  padding: 20px;
+  padding: 2rem;
   height: calc(110vh - 35px);
   background-color: #f9f9f9;
   transition: margin-left 0.3s ease;
 
   @media (min-width: 701px) {
     margin-left: 200px;
+    padding: 2rem 2rem 2rem 2rem;
   }
 
   @media (max-width: 700px) {
     margin-left: 0;
+    padding: 2rem 2rem;
   }
 
   @media (max-width: 320px) {
     height: auto;
+    padding: 1.5rem 1.5rem;
   }
 `;
