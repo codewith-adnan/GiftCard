ff2ab32e9f8e97258f670514f6301b7ec2e2125f
unknown <sendtomadnan@gmail.com>
2025-12-12 15:07:07 +0500

    fix side bar

--- src/layouts/styles.ts
+++ src/layouts/styles.ts
@@ -7,7 +7,7 @@ interface ContentAreaProps {
 export const LayoutWrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 100vh;
+  height: auto;
   overflow: hidden;
 `;
 
@@ -18,19 +18,24 @@ export const ContentArea = styled.div<ContentAreaProps>`
   height: calc(110vh - 35px);
   background-color: #f9f9f9;
   transition: margin-left 0.3s ease;
+  box-sizing: border-box;
+  width: 100%;
 
   @media (min-width: 701px) {
     margin-left: 200px;
     padding: 2rem 2rem 2rem 2rem;
+    width: calc(100% - 200px);
   }
 
   @media (max-width: 700px) {
     margin-left: 0;
     padding: 2rem 2rem;
+    width: 100%;
   }
 
   @media (max-width: 320px) {
     height: auto;
     padding: 1.5rem 1.5rem;
+    width: 100%;
   }
 `;
