44293305234e3fb6c96be57bf02942bbc03e9587
unknown <sendtomadnan@gmail.com>
2025-12-16 14:57:16 +0500

    fix side bar

--- src/pages/users/styles.ts
+++ src/pages/users/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const UsersWrapper = styled.main`
   max-width: 1300px;
   margin: 0 auto;
-  padding: 0;
+  padding: 0 1rem;
   font-family: sans-serif;
   color: #000;
 `;
